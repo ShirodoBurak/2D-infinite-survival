789f1baa12584e36ce8e02318e868ae38968dc68
BurakKaya <mburakkaya@yahoo.com>
2021-10-20 23:20:09 +0300

    Initial commit. Yay!
    Similar to the old "Terraria Like Game" repo. But better ?

---
+++ Assets/TileHolder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileHolder : MonoBehaviour
+{
+    public TileBase missingTexture;
+    public TileBase grass;
+    public TileBase dirt;
+    public TileBase stone;
+}
