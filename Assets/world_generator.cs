e6e0fd88dc6fcb018dc14be5fe2ca2f743914e11
BurakKaya <mburakkaya@yahoo.com>
2021-10-23 02:21:59 +0300

    Success and a cute accident
    I made the generation height infinite. You can go down as much as you want. That is a cute accident.
    
    - Removed Rule Tiles and back to simple tiles.
    - Removed Rule Tile generator
    - Basic camera movement with changeable speed (via scroll)
    - Seems like nothing more...

--- Assets/world_generator.cs
+++ Assets/world_generator.cs
@@ -30,24 +30,35 @@ public class world_generator : MonoBehaviour{
     public Tilemap tilemap;
     public TileHolder tileholder;
     public int additionalHeight = 20;
+    Vector2Int pos;
+    bool first;
+    void Start(){
+        int chunkX = (int)Camera.main.transform.position.x / 16;
+        int chunkY = (int)Camera.main.transform.position.y / 16;
+        pos = new Vector2Int(chunkX, chunkY);
+        first = true;
+    }
     void Update(){
-        if (Input.GetKeyDown(KeyCode.M)) { 
-            GenerateWorld();
+        int chunkX = (int)Camera.main.transform.position.x / 16;
+        int chunkY = (int)Camera.main.transform.position.y / 16;
+        if (pos != new Vector2Int(chunkX, chunkY) || first) {
+            tilemap.ClearAllTiles();
+            GenerateChunk(chunkX, chunkY);
+            pos = new Vector2Int(chunkX, chunkY);
+            first = false;
         }
         else {
             return;
         }
     }
     public int mul = 1;
-    void GenerateWorld(){
-        tilemap.ClearAllTiles();
-
+    void GenerateChunk(int chunkX, int chunkY){
         //First of all, get the count of ignored layers.
         int ignored = countIgnored();
         //If any noise layer exists, execute
         if (_settings.Length > 0){
             //Loop for X value to draw the surface.
-            for (int x = 0; x < 512; x++){
+            for (int x = 0+chunkX*16- getExtraWidth()-5; x < 16+chunkX*16 + getExtraWidth(); x++){
                 float noise1d = 0;
                 for (int k = 0; k < _settings.Length; k++){
                     var settings = _settings[k];
@@ -59,50 +70,44 @@ public class world_generator : MonoBehaviour{
                 }
                 //Find the cave layer
                 int caveLayer = findCaveLayer();
-                for (int y = 0; y < noise1d*mul+additionalHeight; y++){
-                    //Prepare a float variable for cave check.
-                    float noise2d = 0;
-                    float subtract2d = 0;
-                    int cave_layer_amount = 0;
-                    //Loop for X and Y values to generate cave variable.
-                    foreach (var item in _settings){
-                        if (item.noiseType == noise_settings.noise_type.Caves && item.mergeType == noise_settings.merge_type.add ){
-                            noise2d += Mathf.PerlinNoise(
-                        (offset.x + .5f + x + (item.Seed * 128)) * item.size,
-                        (offset.y + .5f + y + (item.Seed * 128)) * item.size) * item.Amplifier;
-                            cave_layer_amount++;
-                        }else if (item.noiseType == noise_settings.noise_type.Caves && item.mergeType == noise_settings.merge_type.subtract && !item.Ingore){
-                            subtract2d+= Mathf.PerlinNoise(
-                        (offset.x + .5f + x + (item.Seed * 128)) * item.size,
-                        (offset.y + .5f + y + (item.Seed * 128)) * item.size) * item.Amplifier;
+                for (int y = 0 + chunkY * 16- getExtraHeight()-5; y < 16+chunkY * 16 + getExtraHeight(); y++){
+                    if (y < noise1d * mul + additionalHeight)
+                    {
+                        //Prepare a float variable for cave check.
+                        float noise2d = 0;
+                        float subtract2d = 0;
+                        int cave_layer_amount = 0;
+                        //Loop for X and Y values to generate cave variable.
+                        foreach (var item in _settings){
+                            if (item.noiseType == noise_settings.noise_type.Caves && item.mergeType == noise_settings.merge_type.add){
+                                noise2d += Mathf.PerlinNoise(
+                            (offset.x + .5f + x + (item.Seed * 128)) * item.size,
+                            (offset.y + .5f + y + (item.Seed * 128)) * item.size) * item.Amplifier;
+                                cave_layer_amount++;
+                            }
+                            else if (item.noiseType == noise_settings.noise_type.Caves && item.mergeType == noise_settings.merge_type.subtract && !item.Ingore){
+                                subtract2d += Mathf.PerlinNoise(
+                            (offset.x + .5f + x + (item.Seed * 128)) * item.size,
+                            (offset.y + .5f + y + (item.Seed * 128)) * item.size) * item.Amplifier;
+                            }
                         }
-                    }
-                    noise2d = noise2d / cave_layer_amount;
-                    noise2d = noise2d - subtract2d;
-                    //Place the tile onto the tilemap
-                    if (noise2d < 0.20f){
-                        placeTile(x, y, tileholder.stone);
+                        noise2d = noise2d / cave_layer_amount;
+                        noise2d = noise2d - subtract2d;
+                        //Place the tile onto the tilemap
+                        if (noise2d < 0.20f){
+                            placeTile(x, y, tileholder.stone);
+                        }
                     }
                 } 
             }
             //Loop through the chunk again to decorate it.
-            Decorate();
+            Decorate(chunkX, chunkY);
         }
     }
-    //This function counts and returns the
-    //number of ignored layers in the array.
-    int countIgnored() {
-        int count = 0;
-        foreach (var settings in _settings){
-            if (settings.Ingore)
-                count++;
-        }
-        return count;
-    }
-    void Decorate()
-    {
+    
+    void Decorate(int chunkX, int chunkY){
         int ignored = countIgnored();
-        for (int x = 0; x < 512; x++){
+        for (int x = 0 + chunkX * 16 - getExtraWidth(); x < 16 + chunkX * 16 + getExtraWidth(); x++){
             float noise1d = 0;
             for (int k = 0; k < _settings.Length; k++){
                 var settings = _settings[k];
@@ -112,43 +117,60 @@ public class world_generator : MonoBehaviour{
                     1) / (_settings.Length - ignored) * settings.Amplifier;
                 }
             }
-            for (int y = Mathf.RoundToInt(noise1d * mul + additionalHeight); y > 0; y--){
+            for (int y = 0 + chunkY * 16 - getExtraHeight(); y < 16 + chunkY * 16 + getExtraHeight(); y++){
                 if (y > noise1d * mul + additionalHeight - (noise1d * 256f)){
                     Vector3Int currentpos = new Vector3Int(x, y, 0);
-                    if (getTile(currentpos) != null){
+
+                    if (getTile(currentpos) != null && y != noise1d * mul + additionalHeight){
+                        placeTile(x, y, tileholder.dirt);
+                    }else if(y==noise1d * mul + additionalHeight-3){
                         placeTile(x, y, tileholder.grass);
                     }
                     else { }
-
                 }
             }
         }
     }
-    //A function that simplifies the tile data checking.
-    TileBase getTile(Vector3Int vec)
+    #region Utility
+    //This function counts and returns the
+    //number of ignored layers in the array.
+    int countIgnored()
     {
+        int count = 0;
+        foreach (var settings in _settings)
+        {
+            if (settings.Ingore)
+                count++;
+        }
+        return count;
+    }
+    //A function that simplifies the tile data checking.
+    TileBase getTile(Vector3Int vec){
         return tilemap.GetTile(vec);
     }
     //A function that simplifies the tile placement.
-    void placeTile(int x, int y, TileBase tile)
-    {
+    void placeTile(int x, int y, TileBase tile){
         tilemap.SetTile(new Vector3Int(x,y,0), tile);
     }
-    int findCaveLayer()
-    {
+    int findCaveLayer(){
         int index = 0;
-        foreach (var item in _settings)
-        {
-            if (item.noiseType != noise_settings.noise_type.Caves)
-            {
+        foreach (var item in _settings){
+            if (item.noiseType != noise_settings.noise_type.Caves){
                 index++;
             }
-            else
-            {
+            else{
                 return index;
             }
         }
         return index;
     }
+    int getExtraWidth(){
+        return (int)Camera.main.pixelWidth / 32 + 3;
+    }
+    int getExtraHeight(){
+        return (int)Camera.main.pixelHeight / 32 + 3;
+    }
+    #endregion
+
 }
 
